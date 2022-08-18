b665453913ec6fa1912316dc62d2f0c75f12f942
천송인 <Csongin@cheonsong-in-ui-MacBookAir.local>
2022-08-18 16:23:34 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,43 @@
-'use strict';
+"use strict";
 
-const hello = (req, res) => {
-  res.render('home/index');
+
+
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    res.render("home/login");
+  },
 };
 
-const login = (req, res) => {
-  res.render('home/login');
+const users = {
+  id: ["csongin", "천코딩", "ccoding"],
+  psword: ["1234", "12345", "123456"],
+};
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
+  },
 };
 
 module.exports = {
-  hello,
-  login,
+  output,
+  process,
 };
